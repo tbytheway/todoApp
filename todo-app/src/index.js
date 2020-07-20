08e1b89fac61fba0081b658c237c7bc49ffadb9d
tbytheway <tdbytheway@gmail.com>
2020-07-20 09:44:15 -0600

    intitial front end

---
+++ todo-app/src/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+
+class App extends Component {
+  constructor() {
+    super()
+    
+    this.state = {}
+
+  }
+
+
+render() {
+  return(
+    <div>
+      <h1>Todo App</h1>
+    </div>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
